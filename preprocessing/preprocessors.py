f638b5dc6c5f8f91d04fde5450cb76a87af2fa4a
Karo Sch <karolineschuster@freenet.de>
2023-08-21 18:14:08 +0200

    implemented backtranslation

--- preprocessing/preprocessors.py
+++ preprocessing/preprocessors.py
@@ -1,6 +1,5 @@
-# import numpy as np
-# import pandas as pd
 import pandas as pd
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 # 27 + 1 emotions from paper on "GoEmotions"
 emotions = ['admiration', 'amusement', 'anger', 'annoyance', 'approval', 'caring', 'confusion', 'curiosity', 'desire',
@@ -247,3 +246,93 @@ def majority_vote(emotion_list: list):
         if freq1 == freq2:
             action = "delete"
     return action, item1
+
+def backtranslate(text, src_tokenizer, src_model, tgt_tokenizer, tgt_model):
+    # Translate source text to the target language
+    src_input = src_tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+    tgt_translation = src_model.generate(**src_input)
+    tgt_translation_text = tgt_tokenizer.decode(tgt_translation[0], skip_special_tokens=True)
+        
+    # Translate target translation back to the source language
+    tgt_input = tgt_tokenizer(tgt_translation_text, return_tensors="pt", padding=True, truncation=True)
+    src_backtranslation = tgt_model.generate(**tgt_input)
+    src_backtranslation_text = src_tokenizer.decode(src_backtranslation[0], skip_special_tokens=True)
+        
+    return src_backtranslation_text #, tgt_translation_text
+
+def bracktranslate_emo(df, language, src_model_name, tgt_model_name):
+
+    src_tokenizer = AutoTokenizer.from_pretrained(src_model_name)
+    src_model = AutoModelForSeq2SeqLM.from_pretrained(src_model_name)
+
+    tgt_tokenizer = AutoTokenizer.from_pretrained(tgt_model_name)
+    tgt_model = AutoModelForSeq2SeqLM.from_pretrained(tgt_model_name)
+
+    # Apply backtranslation to the 'text' column
+    df['text'] = df['text'].apply(lambda x: backtranslate(x, src_tokenizer, src_model, tgt_tokenizer, tgt_model))
+
+    #remove ▁ from subword tokenization
+    df['text'] = df['text'].str.replace("▁", " ")
+
+    # Add "_fr" to the id column for backtranslated rows
+    df['id'] = df['id'] + language
+    
+    return df
+
+def backtranslated_df(df):
+    """
+    create a DataFrame with backtranslated data
+    """
+
+    # two pre-trained translation models: source language and target language
+    src_model_name = ["Helsinki-NLP/opus-mt-en-fr", "Helsinki-NLP/opus-mt-en-de", "Helsinki-NLP/opus-mt-en-es", "Helsinki-NLP/opus-mt-en-da", "Helsinki-NLP/opus-mt-en-sv", "Helsinki-NLP/opus-mt-en-ru", "Helsinki-NLP/opus-mt-en-id", "Helsinki-NLP/opus-mt-en-nl", "Helsinki-NLP/opus-mt-en-cs"]
+    tgt_model_name = ["Helsinki-NLP/opus-mt-fr-en", "Helsinki-NLP/opus-mt-de-en", "Helsinki-NLP/opus-mt-es-en", "Helsinki-NLP/opus-mt-da-en", "Helsinki-NLP/opus-mt-sv-en", "Helsinki-NLP/opus-mt-ru-en", "Helsinki-NLP/opus-mt-id-en", "Helsinki-NLP/opus-mt-nl-en", "Helsinki-NLP/opus-mt-cs-en"]
+    language_short = ["_fr", "_de", "_es", "_da", "_sv", "_ru", "_id", "_nl", "_cs"]
+
+    # Create Backtranslation and concatenate DataFrames
+    embarrassment_fr = bracktranslate_emo(df[df['level0'] == 'embarrassment'], language_short[0], src_model_name[0], tgt_model_name[0])
+    result_df = pd.concat([df, embarrassment_fr], ignore_index=True)
+    relief_fr = bracktranslate_emo(df[df['level0'] == 'relief'], language_short[0], src_model_name[0], tgt_model_name[0])
+    result_df = pd.concat([result_df, relief_fr], ignore_index=True)
+    relief_de = bracktranslate_emo(df[df['level0'] == 'relief'], language_short[1], src_model_name[1], tgt_model_name[1])
+    result_df = pd.concat([result_df, relief_de], ignore_index=True)
+    relief_es = bracktranslate_emo(df[df['level0'] == 'relief'], language_short[2], src_model_name[2], tgt_model_name[2])
+    result_df = pd.concat([result_df, relief_es], ignore_index=True)
+    nervousness_fr = bracktranslate_emo(df[df['level0'] == 'nervousness'], language_short[0], src_model_name[0], tgt_model_name[0])
+    result_df = pd.concat([result_df, nervousness_fr], ignore_index=True)
+    nervousness_de = bracktranslate_emo(df[df['level0'] == 'nervousness'], language_short[1], src_model_name[1], tgt_model_name[1])
+    result_df = pd.concat([result_df, nervousness_de], ignore_index=True)
+    nervousness_es = bracktranslate_emo(df[df['level0'] == 'nervousness'], language_short[2], src_model_name[2], tgt_model_name[2])
+    result_df = pd.concat([result_df, nervousness_es], ignore_index=True)
+    pride_fr = bracktranslate_emo(df[df['level0'] == 'pride'], language_short[0], src_model_name[0], tgt_model_name[0])
+    result_df = pd.concat([result_df, pride_fr], ignore_index=True)
+    pride_de = bracktranslate_emo(df[df['level0'] == 'pride'], language_short[1], src_model_name[1], tgt_model_name[1])
+    result_df = pd.concat([result_df, pride_de], ignore_index=True)
+    pride_es = bracktranslate_emo(df[df['level0'] == 'pride'], language_short[2], src_model_name[2], tgt_model_name[2])
+    result_df = pd.concat([result_df, pride_es], ignore_index=True)
+    pride_da = bracktranslate_emo(df[df['level0'] == 'pride'], language_short[3], src_model_name[3], tgt_model_name[3])
+    result_df = pd.concat([result_df, pride_da], ignore_index=True)
+    pride_sv = bracktranslate_emo(df[df['level0'] == 'pride'], language_short[4], src_model_name[4], tgt_model_name[4])
+    result_df = pd.concat([result_df, pride_sv], ignore_index=True)
+    grief_fr = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[0], src_model_name[0], tgt_model_name[0])
+    result_df = pd.concat([result_df, grief_fr], ignore_index=True)
+    grief_de = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[1], src_model_name[1], tgt_model_name[1])
+    result_df = pd.concat([result_df, grief_de], ignore_index=True)
+    grief_es = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[2], src_model_name[2], tgt_model_name[2])
+    result_df = pd.concat([result_df, grief_es], ignore_index=True)
+    grief_da = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[3], src_model_name[3], tgt_model_name[3])
+    result_df = pd.concat([result_df, grief_da], ignore_index=True)
+    grief_sv = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[4], src_model_name[4], tgt_model_name[4])
+    result_df = pd.concat([result_df, grief_sv], ignore_index=True)
+    grief_ru = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[5], src_model_name[5], tgt_model_name[5])
+    result_df = pd.concat([result_df, grief_ru], ignore_index=True)
+    grief_id = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[6], src_model_name[6], tgt_model_name[6])
+    result_df = pd.concat([result_df, grief_id], ignore_index=True)
+    grief_nl = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[7], src_model_name[7], tgt_model_name[7])
+    result_df = pd.concat([result_df, grief_nl], ignore_index=True)
+    grief_cs = bracktranslate_emo(df[df['level0'] == 'grief'], language_short[8], src_model_name[8], tgt_model_name[8])
+    result_df = pd.concat([result_df, grief_cs], ignore_index=True)
+
+    result_df.to_csv('../data/backtranslated_df.csv', index=False)  # save dataframe as csv
+
+    return result_df 
